f2fd3fde10bef1dcdc8fc295a5a4411ce1817f33
archyta <1931856+archyta@users.noreply.github.com>
2021-09-19 11:06:34 +0800

    delete only ceph volumes

--- del.sh
+++ del.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
-docker volume rm $(docker volume ls)
+docker volume rm $(docker volume ls|grep ceph|awk '{print $2}')
 sudo rm -rf ceph_data/*
 sudo rm -rf ceph_conf/*
 sudo rm -rf osds/osd1/*
